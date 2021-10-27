72d0f5dc3076a7876d24e77d7a27e5a4aad05b45
AYehia0 <ahmedyehia611@gmail.com>
2021-10-27 21:16:45 +0200

    added the routes to the main app

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 require("./models/db")
 const express = require("express")
 
+// the routes
+const studentRouter = require("./routes/studentRoutes")
+const teacherRouter = require("./routes/teacherRoutes")
+
 const app = express()
 
 app.use(express.json())
+
+// using the routes
+app.use(studentRouter)
+app.use(teacherRouter)
 
 module.exports = app
