26e2ea960fa767684043639f80968a663057095a
AYehia0 <ahmedyehia611@gmail.com>
2021-11-01 00:22:41 +0200

    setting the other teacher routes

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -4,5 +4,7 @@ const controller = require("../controller/teacherController")
 // add a teacher :
 // TODO : add a middleware to check if it's the admin
 router.post("/add-teacher", controller.addTeacher)
+router.get("/get-reviews/:id", controller.getAllTeacherReviews)
+router.get("/rate/:id", controller.getTeacherRate)
 
 module.exports = router
