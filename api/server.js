492694fe0ea50fc891ca478564c7d386d3140356
AYehia0 <ahmedyehia611@gmail.com>
2021-11-02 20:01:58 +0200

    re-org files

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ require("dotenv").config()
 const app = require("./app")
 const PORT = process.env.PORT || 8080
 
+console.log(process.env.PORT)
+
 app.listen(PORT, () => {
   console.log(`Server is running on : ${PORT}`)
 })
